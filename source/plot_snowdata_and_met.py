ec7c51d59ad3f4e94208e4404ab3e6590ad949c5
Andy Barrett <apbarret@nsidc.org>
2021-08-03 15:35:02 -0600

    move plot_panel to plotting

--- source/plot_snowdata_and_met.py
+++ source/plot_snowdata_and_met.py
@@ -1,8 +1,6 @@
 """Plots air temperature and snowpack parameters for MOSAiC ROS event"""
-import datetime as dt
 
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 import matplotlib.markers as mmarkers
 import matplotlib.lines as mlines
 
@@ -76,27 +74,6 @@ def mscatter(df, column, ax=None, color='k', size=1, label=None):
     for x, y, m in zip(xs, ys, site_markers):
         if np.isfinite(y):
             ax.scatter(x, y, size, marker=m, c=color, zorder=10, label=label)
-    return ax
-
-
-def plot_panel(ax, fig_label):
-    """Adds a plot panel"""
-    datefmt = mdates.DateFormatter("%d")
-
-    ros_beg = dt.datetime(2020, 9, 13, 10, 0)
-    ros_end = dt.datetime(2020, 9, 14, 9, 40)
-
-    xbeg = dt.datetime(2020, 9, 9, 0)
-    xend = dt.datetime(2020, 9, 16, 0)
-
-    if not ax: ax = plt.gca()
-    ax.set_xlim(xbeg, xend)
-    ax.xaxis.set_major_formatter(datefmt)
-
-    ax.axvspan(ros_beg, ros_end, color='0.8', zorder=0)
-
-    if fig_label: add_fig_label(fig_label, ax)
-    
     return ax
 
 
--- source/plotting.py
+++ source/plotting.py
@@ -1,2 +1,26 @@
 """Common plotting routines"""
+import datetime as dt
+import matplotlib.dates as mdates
+
+
+def plot_panel(ax, fig_label):
+    """Adds a plot panel"""
+    datefmt = mdates.DateFormatter("%d")
+
+    ros_beg = dt.datetime(2020, 9, 13, 10, 0)
+    ros_end = dt.datetime(2020, 9, 14, 9, 40)
+
+    xbeg = dt.datetime(2020, 9, 9, 0)
+    xend = dt.datetime(2020, 9, 16, 0)
+
+    if not ax: ax = plt.gca()
+    ax.set_xlim(xbeg, xend)
+    ax.xaxis.set_major_formatter(datefmt)
+
+    ax.axvspan(ros_beg, ros_end, color='0.8', zorder=0)
+
+    if fig_label: add_fig_label(fig_label, ax)
+    
+    return ax
+
 
