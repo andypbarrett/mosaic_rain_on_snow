54cb1cccdd0e00cf7ca1f8e440ea1c676ceb1a87
Andy Barrett <apbarret@nsidc.org>
2021-08-03 16:21:45 -0600

    add import and skeleton reader

--- plot_microwave.py
+++ plot_microwave.py
@@ -1,1 +1,19 @@
 """Plots radar backscatter and microwave brightness temperature series"""
+import matplotlib.pyplot as plt
+
+import source.reader as reader
+import source.plotting as plotting
+
+
+def plot_microwave():
+    kuka = reader.kukadata()
+    sbr = reader.sbrdata()
+
+    print(kuka.head())
+    print(sbr.head())
+
+    return
+
+
+if __name__ == "__main__":
+    plot_microwave()
