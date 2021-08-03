cd8f6f656d9c2d060e97a200bdbe07d5e8e525f6
Andy Barrett <apbarret@nsidc.org>
2021-08-03 16:10:08 -0600

    add hatching for liquid precipitation

--- source/plotting.py
+++ source/plotting.py
@@ -3,6 +3,18 @@ import datetime as dt
 import matplotlib.dates as mdates
 
 
+TAIR_ABOVE_ZERO = (dt.datetime(2020, 9, 13, 10, 0),
+                   dt.datetime(2020, 9, 14, 9, 40))
+
+RAIN_EVENT_01 = [dt.datetime(2020, 9, 13, 5, 0),
+                 dt.datetime(2020, 9, 13, 10, 0)]
+RAIN_EVENT_02 = [dt.datetime(2020, 9, 14, 1, 0),
+                 dt.datetime(2020, 9, 14, 4, 0)]
+
+XBEGIN = dt.datetime(2020, 9, 9, 0)  # x-axis minimum
+XEND = dt.datetime(2020, 9, 16, 0)   # x-axis maximum
+
+
 def add_fig_label(label, ax):
     ax.text(0.01, 0.98, label,
             transform=ax.transAxes,
@@ -16,17 +28,17 @@ def add_panel(ax, fig_label):
     """Adds a plot panel"""
     datefmt = mdates.DateFormatter("%d")
 
-    ros_beg = dt.datetime(2020, 9, 13, 10, 0)
-    ros_end = dt.datetime(2020, 9, 14, 9, 40)
-
-    xbeg = dt.datetime(2020, 9, 9, 0)
-    xend = dt.datetime(2020, 9, 16, 0)
-
     if not ax: ax = plt.gca()
-    ax.set_xlim(xbeg, xend)
+    ax.set_xlim(XBEGIN, XEND)
     ax.xaxis.set_major_formatter(datefmt)
 
-    ax.axvspan(ros_beg, ros_end, color='0.8', zorder=0)
+    ax.axvspan(TAIR_ABOVE_ZERO[0], TAIR_ABOVE_ZERO[1],
+               color='0.8',
+               zorder=0)
+    ax.axvspan(RAIN_EVENT_01[0], RAIN_EVENT_01[1],
+               hatch='..', fill=False, linestyle='None', zorder=1)
+    ax.axvspan(RAIN_EVENT_02[0], RAIN_EVENT_02[1],
+               hatch='..', fill=False, linestyle='None', zorder=1)
 
     if fig_label: add_fig_label(fig_label, ax)
 
