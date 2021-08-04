79b1f4db60a5b90eee78655c28c824b5bb0e2958
Andy Barrett <apbarret@nsidc.org>
2021-08-04 10:32:25 -0600

    add GridSpec layout and kd_plot func

--- source/plot_microwave.py
+++ source/plot_microwave.py
@@ -1,5 +1,6 @@
 """Plots radar backscatter and microwave brightness temperature series"""
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 
 import reader
 import plotting
@@ -53,6 +54,12 @@ def plot_sbr(df, ax=None, fig_label=None):
     ax.legend(loc="lower left", ncol=2)
 
 
+
+def kd_plot(df, ax=None, fig_label=None):
+    """Creates kernal density plot panel"""
+    if not ax: ax = plt.gca()
+
+
 def split_kuka(kuka, frequency):
     """Split kuka dataframe into Ku and Ka"""
     df = kuka.loc[:,[frequency in col for col in kuka.columns]]
@@ -68,14 +75,25 @@ def plot_microwave():
     ku_df = split_kuka(kuka, "Ku")
     ka_df = split_kuka(kuka, "Ka")
 
-    fig, ax = plt.subplots(3, 1, figsize=(7, 9),
-                           sharex=True,
-                           constrained_layout=True)
-    plot_ku(ku_df, ax=ax[0], fig_label="a) Ku")
-    plot_ka(ka_df, ax=ax[1], fig_label="b) Ka")
-    plot_sbr(sbr, ax=ax[2], fig_label="c) SBR")
-    
+    fig = plt.figure(figsize=(7, 9), constrained_layout=True)
+    gs = GridSpec(3, 5, figure=fig)
+    ax0 = fig.add_subplot(gs[0, :-1])
+    plot_ku(ku_df, ax=ax0, fig_label="a) Ku")
+
+    ax1 = fig.add_subplot(gs[1, :-1], sharex=ax0)
+    plot_ka(ka_df, ax=ax1, fig_label="c) Ka")
+
+    ax2 = fig.add_subplot(gs[2, :-1], sharex=ax0)
+    plot_sbr(sbr, ax=ax2, fig_label="e) SBR")
+
+    # Kernal density plots, following Vishnu's method
+    ax3 = fig.add_subplot(gs[0, 4], sharey=ax0)
+    kd_plot(ku_df, ax=ax3, fig_label="b)")
+
+
+    fig.set_constrained_layout_pads(h_pad=0.01)
     plt.show()
+
     fig.savefig("mosaic_rain_on_snow_microwave.png")
     return
 
