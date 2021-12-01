0fca3eb79b21dc952be16a06ac9e6529f1b8e334
Andy Barrett <apbarret@nsidc.org>
2021-11-30 20:56:09 -0700

    Add pre- and post-event kde panels

--- source/plot_microwave.py
+++ source/plot_microwave.py
@@ -5,6 +5,7 @@ import seaborn as sns
 
 import reader
 import plotting
+from plotting import TAIR_ABOVE_ZERO
 
 
 RADAR_COLORS = ['tab:red',
@@ -42,6 +43,8 @@ SBR_SHADE = [True,
              True,
              False]
 
+PRE_EVENT = TAIR_ABOVE_ZERO[0]
+POST_EVENT = TAIR_ABOVE_ZERO[1]
 
 def plot_ku(df, ax=None, fig_label=None):
     """Plots Ku radar channels"""
@@ -109,33 +112,48 @@ def plot_microwave():
     ka_df = split_kuka(kuka, "Ka")
 
     fig = plt.figure(figsize=(7, 9), constrained_layout=False)
-    gs = GridSpec(3, 5, figure=fig)
-    ax0 = fig.add_subplot(gs[0, :-1])
+    gs = GridSpec(3, 6, figure=fig)
+    ax0 = fig.add_subplot(gs[0, :-2])
     plot_ku(ku_df, ax=ax0, fig_label="a) Ku")
 
-    ax1 = fig.add_subplot(gs[1, :-1], sharex=ax0)
+    ax1 = fig.add_subplot(gs[1, :-2], sharex=ax0)
     plot_ka(ka_df, ax=ax1, fig_label="b) Ka")
     ax1.tick_params(labelbottom=False)
     ax1.set_xlabel('')
 
-    ax2 = fig.add_subplot(gs[2, :-1], sharex=ax0)
+    ax2 = fig.add_subplot(gs[2, :-2], sharex=ax0)
     plot_sbr(sbr, ax=ax2, fig_label="c) SBR")
     ax2.set_xlabel('September 2020')
     
     # Kernal density plots, following Vishnu's method
+    # Ku KDE before event
     ax3 = fig.add_subplot(gs[0, 4], sharey=ax0)
     ax3.tick_params(labelleft=False, left=False,  labelbottom=False)
-    kd_plot(ku_df,
+    kd_plot(ku_df[:PRE_EVENT],
             ku_df.columns,
             RADAR_COLORS,
             RADAR_SHADE,
             RADAR_LINESTYLES,
             ax=ax3,
             fig_label=None)
+    #print(ax3.get_xlim())
 
+    # Ku KDE after event
+    ax6 = fig.add_subplot(gs[0, 5], sharey=ax0)
+    ax6.tick_params(labelleft=False, left=False,  labelbottom=False)
+    kd_plot(ku_df[POST_EVENT:],
+            ku_df.columns,
+            RADAR_COLORS,
+            RADAR_SHADE,
+            RADAR_LINESTYLES,
+            ax=ax6,
+            fig_label=None)
+    #print(ax6.get_xlim())
+
+    # Ka KDE before event
     ax4 = fig.add_subplot(gs[1, 4], sharey=ax1, sharex=ax3)
     ax4.tick_params(labelleft=False, left=False)
-    kd_plot(ka_df,
+    kd_plot(ka_df[:PRE_EVENT],
             ka_df.columns,
             RADAR_COLORS,
             RADAR_SHADE,
@@ -143,13 +161,27 @@ def plot_microwave():
             ax=ax4,
             fig_label=None)
     ax4.set_xlabel('')
-    ax4.set_xticks([0., 0.2])
-    ax4.set_xticklabels(['0', '0.2'])
+    #ax4.set_xticks([0., 0.02])
+    #ax4.set_xticklabels(['0', '0.02'])
 
+    # Ka KDE after event
+    ax7 = fig.add_subplot(gs[1, 5], sharey=ax1, sharex=ax6)
+    ax7.tick_params(labelleft=False, left=False)
+    kd_plot(ka_df[POST_EVENT:],
+            ka_df.columns,
+            RADAR_COLORS,
+            RADAR_SHADE,
+            RADAR_LINESTYLES,
+            ax=ax7,
+            fig_label=None)
+    ax7.set_xlabel('')
+    #ax7.set_xticks([0., 0.02])
+    #ax7.set_xticklabels(['0', '0.02'])
 
+    # SBR KDE before event
     ax5 = fig.add_subplot(gs[2, 4], sharey=ax2)
     ax5.tick_params(labelleft=False, left=False)
-    kd_plot(sbr,
+    kd_plot(sbr[:PRE_EVENT],
             sbr.columns,
             SBR_COLORS,
             SBR_SHADE,
@@ -159,6 +191,18 @@ def plot_microwave():
     ax5.set_xticks([0., 0.02])
     ax5.set_xticklabels(['0', '0.02'])
 
+    # SBR KDE after event
+    ax8 = fig.add_subplot(gs[2, 5], sharey=ax2)
+    ax8.tick_params(labelleft=False, left=False)
+    kd_plot(sbr[POST_EVENT:],
+            sbr.columns,
+            SBR_COLORS,
+            SBR_SHADE,
+            SBR_LINESTYLES,
+            ax=ax8,
+            fig_label=None)
+    ax8.set_xticks([0., 0.02])
+    ax8.set_xticklabels(['0', '0.02'])
 
     fig.subplots_adjust(wspace=0.15)
     plt.show()
