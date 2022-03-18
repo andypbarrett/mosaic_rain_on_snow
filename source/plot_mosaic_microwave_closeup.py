72f199f00942d6b0d1bbdc42465184058ef64fd1
Andy Barrett <apbarret@nsidc.org>
2022-03-17 18:15:44 -0600

    add gridspec defs

--- source/plot_mosaic_microwave_closeup.py
+++ source/plot_mosaic_microwave_closeup.py
@@ -4,9 +4,11 @@ import datetime as dt
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from matplotlib.gridspec import GridSpec
+import seaborn as sns
 
 import reader
-from plot_microwave import split_kuka, plot_ka, plot_ku, plot_sbr
+from plot_microwave import split_kuka, plot_ka, plot_ku, plot_sbr, kd_plot
 
 XBEGIN = dt.datetime(2020,9,12)
 XEND = dt.datetime(2020,9,15)
@@ -28,27 +30,31 @@ def plot_mosaic_microwave_closeup():
     datefmt = mdates.DateFormatter('%d\n%H:%M')
     
     fig = plt.figure(figsize=(7, 9), constrained_layout=False)
+    gs = GridSpec(3, 6, figure=fig)
 
-    ax0 = fig.add_subplot(3, 1, 1)
+    ax0 = fig.add_subplot(gs[0, :-2])
+#    ax0 = fig.add_subplot(3, 1, 1)
     plot_ku(ku_df, ax=ax0, fig_label="a) Ku")
     ax0.tick_params(labelbottom=False)
     ax0.set_xlabel('')
     ax0.set_xlim(XBEGIN, XEND)
 
-    ax1 = fig.add_subplot(3, 1, 2)
+    ax1 = fig.add_subplot(gs[1, :-2], sharex=ax0)
+#    ax1 = fig.add_subplot(3, 1, 2)
     plot_ka(ka_df, ax=ax1, fig_label="b) Ka")
     ax1.tick_params(labelbottom=False)
     ax1.set_xlabel('')
     ax1.set_xlim(XBEGIN, XEND)
 
-    ax2 = fig.add_subplot(3, 1, 3)
+    ax2 = fig.add_subplot(gs[2, :-2], sharex=ax0)
+#    ax2 = fig.add_subplot(3, 1, 3)
     plot_sbr(sbr, ax=ax2, fig_label="c) SBR")
     ax2.set_xlabel('September 2020')
     ax2.set_xlim(XBEGIN, XEND)
     ax2.xaxis.set_major_formatter(datefmt)
 
     plt.show()
-    fig.savefig("mosaic_rain_on_snow_microwave.closeup.png")
+#    fig.savefig("mosaic_rain_on_snow_microwave.closeup.png")
 
     return
 
